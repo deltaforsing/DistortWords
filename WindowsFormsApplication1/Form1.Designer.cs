cc4a7aa89fe9ede82734e58125d4d459813ad760
Максим Румянцев <drunkwolfs1@gmail.com>
2017-05-28 05:57:15 +0400

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.MainTextBox = new System.Windows.Forms.TextBox();
+            this.textBoxPercent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainTextBox
+            // 
+            this.MainTextBox.Location = new System.Drawing.Point(12, 12);
+            this.MainTextBox.Multiline = true;
+            this.MainTextBox.Name = "MainTextBox";
+            this.MainTextBox.Size = new System.Drawing.Size(260, 152);
+            this.MainTextBox.TabIndex = 1;
+            // 
+            // textBoxPercent
+            // 
+            this.textBoxPercent.Location = new System.Drawing.Point(171, 171);
+            this.textBoxPercent.Name = "textBoxPercent";
+            this.textBoxPercent.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPercent.TabIndex = 2;
+            this.textBoxPercent.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBoxPercent);
+            this.Controls.Add(this.MainTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Дангер-каверкатель";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MainTextBox;
+        private System.Windows.Forms.TextBox textBoxPercent;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<string[]> dic = new List<string[]>
+        {
+            new string[] {"ожк","озьг"},
+            new string[] {"кол","га"},
+            new string[] {"ко","га"},
+            new string[] {"колгот","гагот"},
+            new string[] {"шо","ша"},
+            new string[] {"дка","ка"},
+            new string[] {"он","онь"},
+            new string[] {"б","п"},
+            new string[] {"хи","ни"},
+            new string[] {"шк","к"},
+            new string[] {"тро","го"},
+            new string[] {"тка","пка"},
+            new string[] {"кров","кав"},
+            new string[] {"ра","я"},
+            new string[] {"дюк","дю"},
+            new string[] {"ойд","анд"},
+            new string[] {"дка","та"},
+            new string[] {"ро","мо"},
+            new string[] {"ны","ни"},
+            new string[] {"ре","е"},
+            new string[] {"ле","не"},
+            new string[] {"ки","ке"},
+            new string[] {"ш","ф"},
+            new string[] {"шка","вха"},
+            new string[] {"гри","ги"},
+            new string[] {"ч","щ"},
+            new string[] {"ре","ле"},
+            new string[] {"го","хо"},
+            new string[] {"ль","й"},
+            new string[] {"иг","ег"},
+            new string[] {"ра","ва"},
+            new string[] {"к","г"},
+            new string[] {"зо","йо"},
+            new string[] {"зо","ё"},
+            new string[] {"рё","йо"},
+            new string[] {"ск","фк"},
+            new string[] {"ры","вы"},
+            new string[] {"шо","фо"},
+            new string[] {"ло","ле"},
+            new string[] {"сы","фи"},
+            new string[] {"еня","ея"},
+            new string[] {"пель","пюль"},
+            new string[] {"а","я"},
+            new string[] {"у","ю"},
+            new string[] {"о","ё"},
+            new string[] {"ща","ся"},
+            new string[] {"ы","и"},
+            new string[] {"и","й"},
+            new string[] {"ри","ви"},
+            new string[] {"ло","во"},
+            new string[] {"е","и"},
+            new string[] {"и","е"},
+            new string[] {"а","о"},
+            new string[] {"о","а"}
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random gen = new Random();
+            int prob = 0;
+            string tmp = "";
+            List<string> words = new List<string>();
+            words.AddRange(MainTextBox.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            MainTextBox.Text = "";
+            foreach(string str in words)
+            {
+                tmp = str;
+                foreach (string[] str2 in dic)
+                {
+                    if(str.Contains(str2[0]))
+                    {
+                        prob = gen.Next(100);
+                        if (prob <= int.Parse(textBoxPercent.Text))
+                        {
+                            tmp = str.Replace(str2[0], str2[1]);
+                            break;
+                        }
+                    }
+                }
+                MainTextBox.Text += tmp + " ";
+            }
+        }
+    }
+}
